6b3964c6f297a84803256a98f823d2641e180e25
mkoakintomiwa <ibnakintomiwa@gmail.com>
2021-04-10 11:15:17 +0100

    Auto Commit

--- zip-databases.js
+++ zip-databases.js
@@ -8,6 +8,6 @@ let conn = db.createConnection();
 (async _=>{
     let subquery = "";
     if (argv._[0]) subquery = `LIKE '${argv._[0]}'`;
-    console.log(await db.fetch_one(`SHOW DATABASES ${subquery}`,null,conn));
+    console.log(await db.fetch(`SHOW DATABASES ${subquery}`,null,conn));
     db.closeConnection(conn);
 })();
