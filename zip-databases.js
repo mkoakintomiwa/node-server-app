35195a35a5b36eacea40c05d046c885eed99c857
mkoakintomiwa <ibnakintomiwa@gmail.com>
2021-04-10 10:40:36 +0100

    Auto Commit

--- zip-databases.js
+++ zip-databases.js
@@ -5,5 +5,5 @@ let db  = require("./mysql");
 let conn = db.createConnection();
 (async _=>{
     console.log(await db.fetch("SHOW DATABASES",null,conn));
-    db.closeConnection();
+    db.closeConnection(conn);
 })();
