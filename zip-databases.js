51cb6fbfc965a3e1e52e9a351b2a1766534d4d14
mkoakintomiwa <ibnakintomiwa@gmail.com>
2021-04-10 11:13:02 +0100

    Auto Commit

--- zip-databases.js
+++ zip-databases.js
@@ -1,9 +1,13 @@
 let fx = require("./functions");
 let db  = require("./mysql");
+let argv = require("yargs").argv;
+
 
 
 let conn = db.createConnection();
 (async _=>{
-    console.log(await db.fetch_one("SHOW DATABASES",null,conn));
+    let subquery = "";
+    if (!argv._[0]) subquery = `LIKE '${argv._[0]}'`;
+    console.log(await db.fetch_one(`SHOW DATABASES ${subquery}`,null,conn));
     db.closeConnection(conn);
 })();
