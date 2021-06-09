340906d7fb26ee4b085e5b4d8ae3dcdf03df550c
root <root@wpanel.us-central1-a.c.businessnaija.internal>
2021-06-09 23:33:49 +0000

    Auto Commit

--- functions.js
+++ functions.js
@@ -1433,3 +1433,46 @@ var googleAccountAPIAuth = exports.googleAccountAPIAuth = function(emailAddress)
 var UTCDate = exports.UTCDate = function(){
 	return new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '');
 }
+
+
+var zipDatabases = exports.zipDatabases = async function(db_connection){
+
+	const db  = require("./mysql");
+	const argv = require("yargs").argv;
+	const AdmZip = require('adm-zip');
+
+    let subquery = "";
+    if (argv._[0]) subquery = `LIKE '${argv._[0]}'`;
+
+
+    var zip = new AdmZip();
+
+    console.log(await db.all_databases(subquery,db_connection));
+
+    for (let database_name of await db.all_databases(subquery,db_connection)){
+        console.log(`Dumping ${database_name}`);
+        let dump = await fx.shell_exec(`mysqldump ${database_name}`,{
+            hide_output: true
+        });
+        console.log(`Adding ${database_name} to zip archive`);
+        zip.addFile(`databases/${database_name}.sql`,Buffer.alloc(dump.length,dump));
+        fx.println();
+        fx.println();
+    }
+
+    console.log("Writing client...");
+
+    let content = JSON.stringify(fx.mysql_cnf(),null,4);
+
+    zip.addFile(`client.json`,Buffer.alloc(content.length,content));
+
+	let zipFileName = `db-backup-${fx.UTCDate()}.zip`; 
+
+    console.log(`Creating ${zipFileName}`);
+
+    zip.writeZip(`db-backup-${fx.UTCDate()}`);
+
+    fx.println();
+
+	return zipFileName;
+}
--- zip-databases.js
+++ zip-databases.js
@@ -1,44 +1,12 @@
 const fx = require("./functions");
 const db  = require("./mysql");
-const argv = require("yargs").argv;
-const AdmZip = require('adm-zip');
 
 fx.println();
 
 (async _=>{
-    let conn = db.create_connection();
+    let db_connection = db.create_connection();
 
-    let subquery = "";
-    if (argv._[0]) subquery = `LIKE '${argv._[0]}'`;
+    await fx.zipDatabases(db_connection);
 
-
-    var zip = new AdmZip();
-
-    console.log(await db.all_databases(subquery,conn));
-
-    for (let database_name of await db.all_databases(subquery,conn)){
-        console.log(`Dumping ${database_name}`);
-        let dump = await fx.shell_exec(`mysqldump ${database_name}`,{
-            hide_output: true
-        });
-        console.log(`Adding ${database_name} to zip archive`);
-        zip.addFile(`databases/${database_name}.sql`,Buffer.alloc(dump.length,dump));
-        fx.println();
-        fx.println();
-    }
-
-    console.log("Writing client...");
-
-    let content = JSON.stringify(fx.mysql_cnf(),null,4);
-
-    zip.addFile(`client.json`,Buffer.alloc(content.length,content));
-
-    console.log("Creating server-db.zip");
-
-    zip.writeZip(`db-backup-${fx.UTCDate()}`);
-    
-    db.close_connection(conn);
-
-    fx.println();
-    fx.println();
+    db.close_connection(db_connection);
 })();
