b5e8656850665179ff499ba92f02cc5278e8bf64
root <root@wpanel.us-central1-a.c.businessnaija.internal>
2021-06-09 23:06:54 +0000

    Auto Commit

--- mysql.js
+++ mysql.js
@@ -104,7 +104,7 @@ var all_users = exports.all_users = async function(connection){
 
 
 var all_databases = exports.all_databases = async function(subquery="",connection){
-    let rows = await fetch(`SHOW DATABASES ${subquery}`,null,connection);
+    let rows = await fetch(`SELECT schema_name FROM information_schema.schemata ${subquery}`,null,connection);
     console.log(rows);
     let database_names = [];
     let native_database_names = ["information_schema","mysql","performance_schema","sys"];
