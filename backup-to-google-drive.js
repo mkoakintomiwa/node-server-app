23f6db801ce68533408fcbe895c2009613fb1ca3
root <root@wpanel.us-central1-a.c.businessnaija.internal>
2021-06-10 00:13:09 +0000

    Auto Commit

--- backup-to-google-drive.js
+++ backup-to-google-drive.js
@@ -2,46 +2,54 @@ const fx = require("./functions");
 const os = require("os");
 const argv = require("yargs").argv;
 const {google} = require('googleapis');
+const db = require("./mysql");
 
-let emailAddress = argv._[0];
+(async _=>{
+    let emailAddress = argv._[0];
 
-let auth = fx.googleAccountAPIAuth(emailAddress);
+    let auth = fx.googleAccountAPIAuth(emailAddress);
 
-const drive = google.drive({version: 'v3', auth});
+    const drive = google.drive({version: 'v3', auth});
 
-drive.files.list({
-    q: "mimeType='application/vnd.google-apps.folder' and trashed=false and name='server-backups'",
-    fields: 'nextPageToken, files(id, name)',
-    spaces: 'drive'
-}, (err, res) => {
-    if (err) return console.log('The API returned an error: ' + err);
-    const files = res.data.files;
-    if (files.length) {
-        files.map((file) => {
-            var fileMetadata = {
-                'name': 'photo.jpg',
-                'parents':[file.id]
-            }
-              
-            var media = {
-                mimeType: 'image/jpeg',
-                body: fs.createReadStream('files/photo.jpg')
-            };
-              
-            drive.files.create({
-                resource: fileMetadata,
-                media: media,
-                fields: 'id'
-            }, function (err, file) {
-                if (err) {
-                  // Handle error
-                  console.error(err);
-                } else {
-                  console.log('File Id: ', file.id);
+    let db_connection = db.create_connection();
+
+    let zipBackupFileName = await fx.zipDatabases(db_connection);
+
+    drive.files.list({
+        q: "mimeType='application/vnd.google-apps.folder' and trashed=false and name='server-backups'",
+        fields: 'nextPageToken, files(id, name)',
+        spaces: 'drive'
+    }, (err, res) => {
+        if (err) return console.log('The API returned an error: ' + err);
+        const files = res.data.files;
+        if (files.length) {
+            files.map((file) => {
+
+                var fileMetadata = {
+                    'name': zipBackupFileName,
+                    'parents':[file.id]
                 }
+                
+                var media = {
+                    mimeType: 'application/zip',
+                    body: fs.createReadStream(zipBackupFileName)
+                };
+                
+                drive.files.create({
+                    resource: fileMetadata,
+                    media: media,
+                    fields: 'id'
+                }, function (err, file) {
+                    if (err) {
+                    // Handle error
+                        console.error(err);
+                    } else {
+                        console.log('File Id: ', file.id);
+                    }
+                });
             });
-        });
-    } else {
-        console.log('No files found.');
-    }
-});
+        } else {
+            console.log('No files found.');
+        }
+    });
+})();
