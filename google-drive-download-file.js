09ec5b92e2935597e8b2c6d3166f4ebab5e1f084
root <root@wpanel.us-central1-a.c.businessnaija.internal>
2021-06-10 00:57:14 +0000

    Auto Commit

---
+++ google-drive-download-file.js
@@ -0,0 +1,30 @@
+const fx = require("./functions");
+const fs = require("fs");
+const os = require("os");
+const argv = require("yargs").argv;
+const {google} = require('googleapis');
+const db = require("./mysql");
+
+(async _=>{
+    let emailAddress = argv._[0];
+
+    let fileId = argv["file-id"];
+
+    let destPath = argv["dest"];
+
+    let auth = fx.googleAccountAPIAuth(emailAddress);
+
+    const drive = google.drive({version: 'v3', auth});
+
+    var dest = fs.createWriteStream(destPath);
+    
+    drive.files.get({
+        fileId: fileId,
+        alt: 'media'
+    }).on('end', function () {
+      console.log(`Downloaded ${destPath}`);
+    }).on('error', function (err) {
+      console.log('Error during download', err);
+    }).pipe(dest)
+
+})();
