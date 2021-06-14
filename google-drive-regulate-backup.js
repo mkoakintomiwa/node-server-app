64e01a830fa34e4763fcfd0f17c572f584d19364
root <root@wpanel.us-central1-a.c.businessnaija.internal>
2021-06-14 01:18:35 +0000

    Auto Commit

--- google-drive-regulate-backup.js
+++ google-drive-regulate-backup.js
@@ -13,9 +13,7 @@ const process = require("process");
 
     const drive = google.drive({version: 'v3', auth});
 
-   let createdTime = new Date(Date.now() - 60*60*30*1).toISOString();
-
-   console.log(createdTime);
+   let createdTime = new Date(Date.now() - 60*60*30*4).toISOString();
 
     drive.files.list({
         q: `trashed=false and createdTime < '${createdTime}'`,
